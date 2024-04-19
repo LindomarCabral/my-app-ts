4f6bf8ed7617d1330fc083c00539878c6b43ad6c
LindomarCabral <lindomar.tecno@gmail.com>
2024-04-18 22:29:10 -0300

    passando propriedades para componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import  { Card } from './components/Card'
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <>
+    <Header/>
+    <div>
+      hello world
+      <Card 
+        id={1}
+        paragraph='Typescript'
+        details='Ts para frontend e backend'
+      />
+      <Card 
+        id={2}
+        paragraph='HTML'
+        details='HTML para frontend e backend'/>
     </div>
+    <Footer />
+ </>
   );
 }
 
